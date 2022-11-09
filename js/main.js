e3db1630448048b36333f6eb7558c10e38f7465c
PaoloWD <paolo.lore.93@gmail.com>
2022-11-09 17:33:42 +0100

    add reset button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 const btnSbmit = document.querySelector(".btn-primary");
+const btnReset = document.querySelector(".btn-danger");
 const select = document.querySelector("select");
 
 btnSbmit.addEventListener("click", function () {
   const grilContainer = document.querySelector(".gril-container");
   grilContainer.innerHTML = "";
+  //.options[select.selectedIndex]
   const difficulty = select.value;
   const parseDifficulty = parseInt(difficulty);
   const counter = parseDifficulty * parseDifficulty;
@@ -21,4 +23,11 @@ btnSbmit.addEventListener("click", function () {
     });
     grilContainer.append(cell);
   }
+});
+
+btnReset.addEventListener("click", function () {
+  const resetCell = document.querySelectorAll(".square");
+  resetCell.forEach((square) => {
+    square.classList.remove("bg-primary");
+  });
 });
